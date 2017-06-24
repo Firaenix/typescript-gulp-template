003d7a24bf10006d7fe1c127895b710c13c4a6a6
Firaenix <nick.carton@gmail.com>
2017-06-24 12:13:23 +1000

    Initial template commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const ts = require('gulp-typescript');
+
+// pull in the project TypeScript config
+const tsProject = ts.createProject('tsconfig.json');
+
+gulp.task('scripts', () => {
+  const tsResult = tsProject.src()
+  .pipe(sourcemaps.init())
+  .pipe(tsProject());
+  return tsResult.js
+    .pipe(sourcemaps.write('.', {sourceRoot: '.' }))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', ['scripts'], () => {
+  gulp.watch('src/**/*.ts', ['scripts']);
+});
+
+gulp.task('default', ['watch']);
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log('Welcome to the template! This is your entry point!');
